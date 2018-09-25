cd0fa6578f8948420e75e4ee13f256d7c58e396c
Trung <>
2018-09-25 07:19:35 +0700

    Vong doi view controller demo

---
+++ DemoLifeCycle/DetailsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  DetailsViewController.swift
+//  DemoLifeCycle
+//
+//  Created by Trương Thắng on 3/6/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    // - MARK: Việc khởi tạo một class nên để cho bản thân cho Class xử lý.
+    
+    // Các class khác nên biết càng ít về nó càng tốt để đảm bảo tính đóng gói. (Nó không cần biết là storyboard chứa ViewController là gì, identifier của nó là gì...)
+    
+    // Việc khởi tạo ViewController Chỉ cần gọi ViewController.instance là ok
+    
+    static var identifier = "DetailsViewController"
+    static var instance : DetailsViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: DetailsViewController.identifier) as! DetailsViewController
+    }
+    
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+        print("\(type(of: self)) - View Did Load")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("\(type(of: self)) - viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("\(type(of: self)) - viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("\(type(of: self)) - viewWillDisappear")
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("\(type(of: self)) - viewDidDisappear")
+        
+    }
+    
+    deinit {
+        print("\(type(of: self)) - deinit")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+    
+    @IBAction func goBack(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+
+}
